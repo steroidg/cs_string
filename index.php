13e6e20505734bab322f580a556ebff63d25430b
Dinan Yin <steroidg@gmail.com>
2013-03-01 01:03:18 +1100

    use functions in cs_string_functions.php

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
+require ("cs_string_functions.php");
+
 function main() {
-	echo "<html><body>test</body></html>";
+	generate_html_header("boo");
+	sql_connect();
+	get_row_usage();
+	echo "noheunoheuneoh";
+	generate_html_footer();
 };
 
 main();
